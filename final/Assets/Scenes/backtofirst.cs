b641276d3bf17f1575af66f97cb8a8739bef8fff
1062994 <s1062994@mail.ncyu.edu.tw>
2020-09-25 01:01:02 +0800

    project

---
+++ final/Assets/Scenes/backtofirst.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backtofirst : MonoBehaviour
+{
+    public GameObject p, person;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(person.transform.position.y<2)
+        {
+            Vector3 pos= new Vector3(-0.03f, 9.74f, 198.94f);
+            
+            person.transform.position=pos;
+        }
+    }
+}
---
+++ final/Assets/Scenes/cubemove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class cubemove : MonoBehaviour
+{
+    public GameObject cube1, cube2, cube3;
+    int count = 0,first=1;
+    float x=0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if(count==2500&&first==1)
+        {
+            x = -x;
+            count = 0;
+            first = 0;
+        }
+        if(count==5000&&first==0)
+        {
+            x = -x;
+            count = 0;
+        }
+        cube1.transform.Translate(x, 0, 0);
+        cube2.transform.Translate(-x, 0, 0);
+        cube3.transform.Translate(x, 0, 0);
+        count++;
+        
+    }
+}
